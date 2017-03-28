55130d1880de3a00b64044ebbe5870dafbec1a69
Arun <arun4aue@gmail.com>
2017-03-28 15:05:22 +0530

    Update server.js
    API Basejump: Timestamp microservice
    User stories:
    1) I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016)
    2) If it does, it returns both the Unix timestamp and the natural language form of that date.
    3) If it does not contain a date or Unix timestamp, it returns null for those properties.

--- API-Projects/TimestampMicroservice/server.js
+++ API-Projects/TimestampMicroservice/server.js
@@ -1,1 +1,27 @@
+var express = require('express');
+var app = express()
+var unixTime = require('unix-time');
+app.get('/:param1', (req,res)=>{
+    var url = req.url;
+    var param1 = req.params.param1;
+    var bar = {
+        unix : this.unixvalue,
+        natural : this.naturalvalue
+    }
+    if(req)
+    if(isNaN(param1) && url != '/favicon.ico'){
+        var unixdate = unixTime(new Date(param1));        
+        bar.unixvalue = unixdate,
+        bar.naturalvalue = param1
+    } else if(!isNaN(param1) && url != '/favicon.ico'){ 
+        bar.unixvalue = param1
+        var d = new Date(param1*1000)
+        var months = ["January","February","March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        bar.naturalvalue = months[d.getMonth()] + " " + d.getDate() + "," + d.getFullYear(); 
 
+    }
+    res.send(bar)   
+})
+app.listen(3000,()=>{
+    console.log("App listening at 3000")
+})
